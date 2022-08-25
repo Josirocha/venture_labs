12a6e3b07b4489037959883b694f553ef669bd17
Josiane Rocha <josi1992@gmail.com>
2022-08-25 18:07:35 -0300

    chore: initial configuration and structuring of the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Routes from './Routes/Routes'
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Routes = () => {
+    return (
+            <BrowserRouter>
+                <Routes>
+                    <Route />
+                    <Route />
+                </Routes>
+            </BrowserRouter>
+    )
+}
+
+export default Routes
